460feed9b30e5d284d4dddfb1d5553cc5c2ad339
prothegee <prothegee@proton.me>
2025-10-31 15:17:58 +0700

    updating build script

--- build-all-debug.sh
+++ build-all-debug.sh
@@ -2,12 +2,12 @@ cmake   -S . -B build/debug \
         -G "Ninja" \
         -DCMAKE_BUILD_TYPE=Debug \
         -DCMAKE_CXX_STANDARD=23 \
-        -DCMAKE_CXX_FLAGS="-std=c++23" \
+        -DCMAKE_CXX_FLAGS="-std=c++23 -fPIC" \
         -DCMAKE_EXPORT_COMPILE_COMMANDS=1 \
         -DPHH_BUILD_TEST=ON \
         -DPHH_BUILD_TOOL=ON;
 
-ln -sf build/debug/compile_commands.json;
+ln -sf build/debug/compile_commands.json ./;
 
 cmake --build build/debug;
 
