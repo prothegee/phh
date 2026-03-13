12fa7d5c72ee978aa916bea375c0b80493e97c4f
prothegee <prothegee@proton.me>
2026-03-13 19:35:34 +0700

    adding gen header

---
+++ inc/phh/utils/gen.hh
@@ -0,0 +1,45 @@
+/// ### file: gen
+///
+/// ### author: Prana Ron
+///
+/// ---------------------------------------------------------
+///
+/// ### brief:
+/// n/a
+///
+/// ### note:
+/// n/a
+///
+/// ---------------------------------------------------------
+///
+/// ### copyright:
+///
+/// - (c) 2026 Prana Ron // @prothegee // prothegee@proton.me
+///
+/// ### contributors:
+///
+/// - Prana Ron // @prothegee // prothegee@proton.me
+///
+#ifndef PHH_UTILS_GEN_HH
+#define PHH_UTILS_GEN_HH
+#include <cstddef>
+#include <cstdint>
+
+namespace phh {
+namespace utils {
+namespace gen {
+
+/// @brief generate random bytes to dst param
+///
+/// @param dst - destination data pointer
+void secure_random_bytes(uint8_t *dst, size_t len);
+
+uint64_t unix_timestamp_ms();
+
+uint64_t monotonic_counter();
+
+} // namespace gen
+} // namespace utils
+} // namespace phh
+
+#endif // PHH_UTILS_GEN_HH
